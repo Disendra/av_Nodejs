4329cd13f6d3034fd8aa90682993a16d17295037
Disendra <disendra@889>
2024-05-22 10:08:55 +0530

    22/5/2024

--- nodeJs/socialLogin.js
+++ nodeJs/socialLogin.js
@@ -18,7 +18,7 @@ router.use(cors());
 let jwtToken;
 let destination;
 let userEmailId;
-let url = 'http://192.168.0.6:4500/redirected-page';
+let url = 'http://10.0.0.68:4500/redirected-page';
 
 passport.serializeUser((user, done) => {
   done(null, user);
